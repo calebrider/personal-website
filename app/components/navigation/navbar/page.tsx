8a1fb08579265f2478c10dcf1fc4984e4fa76ec9
Caleb Rider <caleb.j.rider@gmail.com>
2023-10-20 16:38:57 -0400

    fixed undefined reading map error

--- app/components/navigation/navbar/page.tsx
+++ app/components/navigation/navbar/page.tsx
@@ -6,7 +6,7 @@ export default function Navbar(props: any) {
       <>
         <div className="flex justify-end items-center sm:justify-center object-top bg-blue-600 h-20 w-full">
             <ul className="flex items-center">
-                {props.linkText.map((text: string) => (
+                {(props.linkText ?? []).map((text: string) => (
                     <li key={text}>
                         <Link href={props.hrefSymbol + text.toLowerCase()}>
                             <span className="flex items-center justify-center w-24 h-20 px-2 hover:bg-blue-700 duration-200 ease-in-out text-white">{text}</span>
--- app/components/navigation/navdrop/page.tsx
+++ app/components/navigation/navdrop/page.tsx
@@ -9,6 +9,7 @@ export default function Navdrop(props: any) {
     const [isOpen, setIsOpen] = useState(false)
     const handleClick = () => {
         setIsOpen(!isOpen)
+        console.log(isOpen)
     }
 
     return (
@@ -18,9 +19,9 @@ export default function Navdrop(props: any) {
                 <Hamburger toggled={isOpen} toggle={handleClick} rounded color="#fff"/>
             </div>
         </div>
-        <div className={`absolute w-full max-h-96 bg-blue-600 rounded-b-lg overflow-hidden transition-height duration-500 ease-in-out ${isOpen ? "max-h-96" : "max-h-0"}`}>
+        <div className={`absolute w-full max-h-0 bg-blue-600 rounded-b-lg overflow-hidden transition-height duration-500 ease-in-out ${isOpen ? "max-h-96" : "max-h-0"}`}>
             <ul className="flex flex-col items-center">
-                {props.linkText.map((text: string) => (
+                {(props.linkText ?? []).map((text: string) => (
                     <li key={text}>
                         <Link href={props.hrefSymbol + text.toLowerCase()} onClick={handleClick}>
                             <span className="flex items-center justify-center w-screen h-16 px-2 hover:bg-blue-700 text-white">{text}</span>
