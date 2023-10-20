868dabe48709f467088f0d9116c1a68533681e1f
Caleb Rider <caleb.j.rider@gmail.com>
2023-10-20 16:19:45 -0400

    added hamburger dropdown functionality

--- app/components/navigation/page.tsx
+++ app/components/navigation/page.tsx
@@ -2,13 +2,16 @@ import Navbar from "./navbar/page";
 import Navdrop from "./navdrop/page";
 
 export default function Navigation() {
+  
+  var linkText = ["About", "Skills", "Projects", "Contact"]
+
     return (
       <div className="sticky top-0 z-50 w-full">
         <div className="hidden sm:contents">
-          <Navbar/>
+          <Navbar linkText={linkText} hrefSymbol="#"/>
         </div>
         <div className="contents sm:hidden">
-          <Navdrop/>
+          <Navdrop linkText={linkText} hrefSymbol="#"/>
         </div>
       </div>
     )
