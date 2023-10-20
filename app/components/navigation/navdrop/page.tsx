8b0231eae78a3f8f05ee1dfaf271d239ec6d09f9
Caleb Rider <caleb.j.rider@gmail.com>
2023-10-20 12:14:37 -0400

    removed unused code from Navdrop

--- app/components/navigation/navdrop/page.tsx
+++ app/components/navigation/navdrop/page.tsx
@@ -1,18 +1,16 @@
 "use client";
 
-import Link from "next/link";
 import Hamburger from "hamburger-react";
 import { useState } from "react";
 
 
 export default function Navdrop() {
-    var linkText = ["About", "Skills", "Projects", "Contact"]
-    var hrefSymbol: string = "#"
-
+    
     const [isOpen, setIsOpen] = useState(false)
     const handleClick = () => {
         setIsOpen(!isOpen)
     }
+
     return (
       <>
         <div className="flex justify-end items-center sm:justify-center object-top bg-blue-600 h-20 w-full">
