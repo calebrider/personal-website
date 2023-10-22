6dc377d9377f97ea8f04e370b3d4d0a88d1e9440
Caleb Rider <caleb.j.rider@gmail.com>
2023-10-21 20:57:51 -0400

    modified component spacing/responsiveness

--- app/components/about/page.tsx
+++ app/components/about/page.tsx
@@ -2,29 +2,25 @@ import Image from "next/image"
 
 export default function About() {
     return (
-        <div id="about" className="scroll-mt-20">
-            <div className="flex justify-center w-full">
-                <h1 className="text-6xl tracking-wide pt-10">About</h1>
+        <div id="about" className="flex flex-col scroll-mt-20 w-5/6">
+            <div className="flex pt-10 justify-end">
+                <h1 className="text-2xl tracking-wide text-gray-500">About</h1>
             </div>
-            <div className="flex flex-col items-center justify-around pb-10">
-                <div className="flex flex-col space-y-4 max-w-sm lg:max-w-lg p-10">
-                    <h1 className="text-4xl tracking-wide">Caleb Rider</h1>
-                    <h2 className="text-xl tracking-wide">Software Engineer</h2>
-                    <p>Hi! I&apos;m a software engineer based out of Richmond, VA.
-                    I&apos;ve been working professionally since May 2020,
-                    and most of my professional software experience consists of
-                    back-end development in both C# (.NET) and Python.
-                    I also enjoy front-end development in my spare time.
-                    </p>
+            <div className="flex flex-col lg:flex-row lg:justify-center items-center content-start lg:content-center py-8 lg:py-16 gap-8 lg:gap-24">
+                <div className="flex justify-end lg:w-1/2">
+                    <div className="flex flex-col">
+                        <h1 className="text-4xl tracking-wide">Caleb Rider</h1>
+                        <h2 className="pt-2 text-xl tracking-wide">Software Engineer</h2>
+                        <p className="pt-5 max-w-sm">Hello! I&apos;m a software engineer based out of Richmond, VA.
+                        I&apos;ve been working professionally since May 2020,
+                        and most of my professional software experience consists of
+                        back-end development in both C# (.NET) and Python.
+                        I also enjoy front-end development in my spare time.
+                        </p>
+                    </div>
                 </div>
-                <div className="relative w-52 h-52 sm:w-72 sm:h-72">
-                    <Image
-                        className="rounded-full w-auto"
-                        src="/headshot.jpg"
-                        alt="Headshot"
-                        fill={true}
-                        priority
-                    />
+                <div className="flex justify-center lg:justify-start lg:w-1/2">
+                    <Image src="/headshot.jpg" alt="Headshot" width={400} height={400} className="max-w-sm h-auto w-full shadow-lg rounded-full"/>
                 </div>
             </div>
         </div>
