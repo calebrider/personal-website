ba0412b2776937662eb7a95fc937bcb402a6db94
Caleb Rider <caleb.j.rider@gmail.com>
2023-10-22 21:53:29 -0400

    add empty prop values

--- app/components/projects/project/page.tsx
+++ app/components/projects/project/page.tsx
@@ -9,13 +9,13 @@ export default function Project(props: any) {
             <div className="h-3/5">
                 <h3 className="px-8 pt-2 text-start text-gray-900">Technologies Used:</h3>
                 <ul className="px-12 py-2">
-                    {props.technologies.map((technology: string) => (
+                    {(props.technologies ?? []).map((technology: string) => (
                         <li key={technology} className="text-primary py-1">• {technology}</li>
                     ))}
                 </ul>
             </div>
             <div className="flex justify-center h-1/5">
-                <Link href={props.url} className="h-1/2 w-1/2 text-center py-2 text-white rounded-full transition-all duration-500 bg-gradient-to-l from-primary via-accent to-primary bg-size-200 bg-pos-100 hover:bg-pos-0">Hover me</Link>
+                <Link href={(props.url ?? "")} className="h-1/2 w-1/2 text-center py-2 text-white rounded-full transition-all duration-500 bg-gradient-to-l from-primary via-accent to-primary bg-size-200 bg-pos-100 hover:bg-pos-0">Hover me</Link>
             </div>
         </div>
     )
